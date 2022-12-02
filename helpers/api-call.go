7a9872b8549da0f6f4d2fb02c1558591f0986d0b
sirkorben <cartelside@gmail.com>
2022-12-02 13:26:45 +0200

    unnessecary code removed

--- helpers/api-call.go
+++ helpers/api-call.go
@@ -8,28 +8,6 @@ import (
 	"net/http"
 )
 
-type JokeResponse struct {
-	ID     string `json:"id"`
-	Joke   string `json:"joke"`
-	Status int    `json:"status"`
-}
-
-type Response struct {
-	Prices []*Oneprice
-}
-type Oneprice struct {
-	Price            float64 `json:"price,omitempty"`
-	Article          string  `json:"article,omitempty"`
-	Supplier         string  `json:"supplier,omitempty"`
-	SupplierPriceNum float64 `json:"supplier_price_num,omitempty"`
-	Brand            string  `json:"brand,omitempty"`
-	Currency         string  `json:"currency,omitempty"`
-	CurrencyRate     string  `json:"currency_rate,omitempty"`
-	Delivery         string  `json:"delivery,omitempty"`
-	Weight           float64 `json:"weight,omitempty"`
-	Name             string  `json:"name,omitempty"`
-}
-
 func CallForPrices(article string, dst interface{}) error {
 	//TODO: hide apiKey in env vars
 	apiKey := "f22d9a0a6a65a45e5fe9cd652deb0e98e9051b286d36709ae12d1900da516c10"
@@ -40,6 +18,7 @@ func CallForPrices(article string, dst interface{}) error {
 		log.Println("ERROR IN WRAPPING REQUEST TO API", err)
 		return err
 	}
+
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/json")
 	resp, err := client.Do(req)
@@ -56,16 +35,11 @@ func CallForPrices(article string, dst interface{}) error {
 		return err
 	}
 
-	// taking a pointer variable using
-	// := by assigning it with the
-	// address of variable models.ApiResponse{}
-	// apiResponse := &models.ApiResponse{}
 	json.Unmarshal(bodyBytes, &dst)
 	if err != nil {
 		log.Println("REQUEST BODY UNMARSHALLING err ", err)
 		return err
 	}
 
-	// returning pointer variable to the address of models.ApiResponse{}
 	return nil
 }
